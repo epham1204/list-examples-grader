2444fc37a7ddc1423d2daaeeef6c721cb77e355f
Ethan Pham <72236910+epham1204@users.noreply.github.com>
2024-03-07 12:35:30 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -8,7 +8,36 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [[ -f student-submission/ListExamples.java ]]
+then
+    echo "File Found"
+else
+    echo "File ListExamples.java not found"
+    exit 1
+fi
 
+cp student-submission/ListExamples.java grading-area/
+cp TestListExamples.java grading-area/
+cp -r lib grading-area
+
+cd grading-area
+javac -cp $CPATH *.java
+
+if [[ $? -ne 0 ]]
+then
+    echo "Compilation error!"
+    exit 1
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+
+echo "Program compiled successfully!"
+
+# lastline= $(cat junit-output.txt | tail -n 2 | head -n 1)
+tests=$(cat junit-output.txt | tail -n 2 | head -n 1 | awk -F '[, ]' '{print $3}')
+failures=$(cat junit-output.txt | tail -n 2 | head -n 1 | awk '{print $5}')
+successes=$(($tests - $failures))
+echo "$successes / $tests"
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
